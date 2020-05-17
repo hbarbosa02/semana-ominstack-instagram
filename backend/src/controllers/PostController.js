13a57756f6d5eea65a53cc62db06cb1313841fba
hiran.g.b.silva <hiran.g.b.silva@gmail.com>
2020-05-17 10:59:19 -0300

    redimensionamento das imagens para o frontend

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -1,3 +1,7 @@
+const sharp = require("sharp");
+const path = require("path");
+const fs = require("fs");
+
 const Post = require("../models/Post");
 
 module.exports = {
@@ -9,6 +13,13 @@ module.exports = {
   async store(req, res) {
     const { author, place, description, hashtags } = req.body;
     const { filename: image } = req.file;
+
+    await sharp(req.file.path)
+      .resize(500)
+      .jpeg({ quality: 70 })
+      .toFile(path.resolve(req.file.destination, "resized", image));
+
+    fs.unlinkSync(req.file.path);
 
     const post = await Post.create({
       author,
