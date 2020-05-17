bd3f3a005faa1e4378a6ce97dde517f8dbcb582a
hiran.g.b.silva <hiran.g.b.silva@gmail.com>
2020-05-17 11:09:41 -0300

    finalizando backend

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -14,10 +14,13 @@ module.exports = {
     const { author, place, description, hashtags } = req.body;
     const { filename: image } = req.file;
 
+    const [name] = image.split(".");
+    const fileName = `${name}.jpg`;
+
     await sharp(req.file.path)
       .resize(500)
       .jpeg({ quality: 70 })
-      .toFile(path.resolve(req.file.destination, "resized", image));
+      .toFile(path.resolve(req.file.destination, "resized", fileName));
 
     fs.unlinkSync(req.file.path);
 
@@ -26,8 +29,10 @@ module.exports = {
       place,
       description,
       hashtags,
-      image,
+      image: fileName,
     });
+
+    req.io.emit("post", post);
 
     return res.json(post);
   },
