f1877b278d485090e8b8d7c4fb9d02b42f4e3af2
hiran.g.b.silva <hiran.g.b.silva@gmail.com>
2020-05-17 15:10:43 -0300

    finalizando projeto frontend

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import io from "socket.io-client";
+
+import api from "../services/api";
 
 import "./Feed.css";
 
@@ -7,21 +10,71 @@ import like from "../assets/like.svg";
 import comment from "../assets/comment.svg";
 import send from "../assets/send.svg";
 class Feed extends Component {
+  state = {
+    feed: [],
+  };
+
+  async componentDidMount() {
+    this.registerToSocket();
+    const response = await api.get("posts");
+
+    this.setState({ feed: response.data });
+  }
+
+  registerToSocket = () => {
+    const socket = io("http://localhost:4000");
+
+    socket.on("post", (newPost) => {
+      this.setState({ feed: [newPost, ...this.state.feed] });
+    });
+
+    socket.on("like", (likedPost) => {
+      this.setState({
+        feed: this.state.feed.map((post) =>
+          post._id === likedPost._id ? likedPost : post
+        ),
+      });
+    });
+  };
+
+  handleLike = (id) => {
+    api.post(`/post/${id}/like`);
+  };
+
   render() {
     return (
       <section id="post-list">
-        <article>
-          <header>
-            <div className="user-info">
-              <span>Hiran Gondim</span>
-              <span className="place">Natal RN</span>
-            </div>
+        {this.state.feed.map((post) => (
+          <article key={post._id}>
+            <header>
+              <div className="user-info">
+                <span>{post.author}</span>
+                <span className="place">{post.place}</span>
+              </div>
 
-            <img src={more} alt="Mais" />
-          </header>
+              <img src={more} alt="Mais" />
+            </header>
 
-          <img src="http://localhost:4000/file/271028.jpg" alt="" />
-        </article>
+            <img src={`http://localhost:4000/files/${post.image}`} alt="" />
+
+            <footer>
+              <div className="actions">
+                <button type="button" onClick={() => this.handleLike(post._id)}>
+                  <img src={like} alt="Like" />
+                </button>
+                <img src={comment} alt="Comment" />
+                <img src={send} alt="Send" />
+              </div>
+
+              <strong>{post.likes}</strong>
+
+              <p>
+                {post.description}
+                <span>{post.hashtags}</span>
+              </p>
+            </footer>
+          </article>
+        ))}
       </section>
     );
   }
