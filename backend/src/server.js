e2dd39926a4ab94be31e87c5f0942dc2adf3d1cd
hiran.g.b.silva <hiran.g.b.silva@gmail.com>
2020-05-17 10:18:19 -0300

    Criando arquivo de rotas e configurando o mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://admin:admin@cluster0-qm1bp.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.use(require("./routes"));
 
 app.listen(4000);
